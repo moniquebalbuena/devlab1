f83a5843704c361102f54089933b003b95bccf86
Monique Balbuena <mbalbuena93@gmail.com>
2018-06-12 20:03:07 +0000

    Inital Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*global $*/
+$(document).ready(function(){
+    
+var contact1 = "contact.html";
+var me1 = "aboutme.html";
+var portfolio = "port.html";
+
+$("#email").click(function(){
+    window.location = contact1;
+});
+
+$("#me").click(function(){
+    window.location = me1
+});
+
+$("#port").click(function(){
+    window.location = portfolio
+});
+
+})
